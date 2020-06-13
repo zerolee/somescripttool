25b17ddce57a5e3cc4e333936644d63ee4092095
zerolee <zmqclee@gmail.com>
2020-06-13 16:33:59 +0800

    mycentos: 添加关机选项

--- mycentos-completion.sh
+++ mycentos-completion.sh
@@ -5,7 +5,7 @@ function _foo() {
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    opts="help ls boot ssh status ping pause resume ss"
+    opts="help ls boot ssh status ping pause resume ss shutdown poweroff"
 
     COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
     
